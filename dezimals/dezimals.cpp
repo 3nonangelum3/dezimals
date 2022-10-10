7e1a4b4326bab97fe9491aae5c145ce964dc4148
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-10-10 23:02:08 +0200

    saves

--- dezimals/dezimals.cpp
+++ dezimals/dezimals.cpp
@@ -3,14 +3,14 @@ using std::string;
 using std::cout;
 using std::cin;
 
-bool findSequence(string fraction) {
-	return true;
+bool findSequence(string fraction, string $sequence) {
+	
 }
 
 int main()
 {
-	/*int counter = 0, denominator = 0;
-	string result = "";
+	int counter = 0, denominator = 0;
+	string result = "", sequence;
 	do {
 		cout << "Zahler/Counter: ";
 		cin >> counter;
@@ -23,22 +23,7 @@ int main()
 		cin.clear();
 		cin.ignore(2e5, '\n');
 	} while (denominator && denominator > 0 && !cin.fail());
-	while (!findSequence(result)) {
+	while (!findSequence(result, sequence)) {
 
-	}*/
-	for (int i = 1; i < 1898; i++)
-	{
-		if (!(1898 % i))
-		{
-			cout << i << " ";
-		}
-	}
-	cout << '\n';
-	for (int i = 1; i < 925; i++)
-	{
-		if (!(925 % i))
-		{
-			cout << i << ' ';
-		}
 	}
 }
